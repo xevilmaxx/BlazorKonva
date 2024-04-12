258cc61d67d4985b5d667827149517594e3f6854
xevilmaxx <prova705@gmail.com>
2024-04-12 11:07:04 +0200

    fix api forget

--- BlazorKonva/KonvaClasses/Container/KonvaContainerConfigsDTO.cs
+++ BlazorKonva/KonvaClasses/Container/KonvaContainerConfigsDTO.cs
@@ -1,8 +1,27 @@
 ﻿using BlazorKonva.KonvaClasses.Node;
+using System.Numerics;
+using System;
+using System.Text.Json.Serialization;
 
 namespace BlazorKonva.KonvaClasses.Container
 {
     public class KonvaContainerConfigsDTO : KonvaNodeConfigsDTO
     {
+
+        [JsonPropertyName("clipX")]
+        public int? ClipX { get; set; }
+
+        [JsonPropertyName("clipY")]
+        public int? ClipY { get; set; }
+
+        [JsonPropertyName("clipWidth")]
+        public int? ClipWidth { get; set; }
+
+        [JsonPropertyName("clipHeight")]
+        public int? ClipHeight { get; set; }
+
+        [JsonPropertyName("clipFunc")]
+        public object? ClipFunc { get; set; }
+        
     }
 }
